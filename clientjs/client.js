2838c4d36e713534029933124684c177b93a9f9a
ayush Panwar <ayushpanwar691@gmail.com>
2022-09-10 21:04:53 +0530

    Chattipie frontend

---
+++ clientjs/client.js
@@ -0,0 +1,35 @@
+//const io=require("socket.io-client")
+const socket= io('https://chattipie.herokuapp.com', {transports: ['websocket', 'polling', 'flashsocket']});
+const form= document.getElementById('send-container');
+const messageInput=document.getElementById('messageInp');
+const messageContainer=document.querySelector(".container");
+var audio=new Audio('notification.wav');
+const append=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left'){
+    audio.play();
+    }
+}
+form.addEventListener('submit',(e)=>{
+ e.preventDefault();
+ var message= messageInput.value;
+ append(`You: ${message}`,'right')
+ socket.emit('send',message);
+ messageInput.value='';
+});
+const name=prompt("Enter your name to join");
+
+socket.emit('new-user-joined',name);
+socket.on('user-joined',name=>{
+  append(`${name} joined the chat`,'right')      
+})
+socket.on('receive',data=>{
+    append(`${data.name}: ${data.message}`,'left')
+})
+socket.on('leave',data=>{
+    append(`${data.name} left the chat`, 'right')
+})
